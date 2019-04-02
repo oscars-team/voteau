8c4e239b081d53322ab3d0592d955930cca19a6c
15396910206 <183604002@qq.com>
2019-04-02 10:10:40 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    singleques:[
+      { name:'1、你最喜欢的政党是那个？'  },
+      { name: '2、你最喜欢的首相是那个？' },
+    ],
+    duo:[
+      { name: '我个政党你喜欢那几个' }
+    ],
+    slngleanswer:[
+       {name:'工党',name:'自由党'},
+       {name:'莫里森',name:'薛顿'}
+    ],
+    duoanswer:[
+      
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
